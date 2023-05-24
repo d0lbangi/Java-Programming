dffb6515a6510eb274091eed8e918836beb47f21
Jeongho Park <d0lbangi13@naver.com>
2023-05-25 03:10:59 +0900

    added conditionalStatement Part

--- src/main/java/com/example/test/VariousOperations.java
+++ src/main/java/com/example/test/VariousOperations.java
@@ -79,27 +79,27 @@ public class VariousOperations {
         result2 = num11 & num22;
         System.out.println("result = " + result2);
         System.out.println(Integer.toBinaryString(num11));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
 
 //      OR 연산자 (|)
         System.out.println("== | ==");
 
         result2 = num11 | num22;
         System.out.println("result = " + result2);
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
 
 //      XOR 연산자 (^)
         System.out.println("== XOR ==");
 
         result = num1 ^ num2;
         System.out.println("result2 = " + result2);
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num22)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result2)));
 
 //      반전 연산자 (~)
         System.out.println("== ~ ==");
@@ -107,34 +107,34 @@ public class VariousOperations {
 
         result2 = ~num11;
         System.out.println("result2 = " + result2);
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
-        System.out.println("%04d\n", Integer.toBinaryString(num11));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(num11)));
+//        System.out.println("%04d\n", Integer.toBinaryString(num11));
 
 //      비트 이동 연산자
         System.out.println("== 비트 이동 연산자 ==");
 //      << 연산자
         int numAA = 3;
-        result33 = numAA << 1;
+//        result33 = numAA << 1;
 
         System.out.println("result2 = " + result2);
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(numAA)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(numAA)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
 //      >> 연산자
-        result == numAA >> 1;
+//        result == numAA >> 1;
 
         System.out.println("result2 = " + result2);
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(numAA)));
-        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(numAA)));
+//        System.out.println("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
 //      >>> 연산자
         numA = -5;
         result = numA >> 1;
-        System.out.println("%s\n", Integer.toBinaryString((numA));
-        System.out.println("%s\n", Integer.toBinaryString((result));
+//        System.out.println("%s\n", Integer.toBinaryString((numA));
+//        System.out.println("%s\n", Integer.toBinaryString((result));
 
         result = numA >>> 1;
-        System.out.println("%s\n", Integer.toBinaryString((numA));
-        System.out.println("%s\n", Integer.toBinaryString((result));
+//        System.out.println("%s\n", Integer.toBinaryString((numA));
+//        System.out.println("%s\n", Integer.toBinaryString((result));
     }
 }
