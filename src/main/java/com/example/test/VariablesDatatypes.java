69bd587872c2fa2760d2064fe569302a0cfa96e5
Jeongho Park <d0lbangi13@naver.com>
2023-05-25 02:00:16 +0900

    added put, get, size, remove, containsKey, Generics on the form of DataType

--- src/main/java/com/example/test/VariablesDatatypes.java
+++ src/main/java/com/example/test/VariablesDatatypes.java
@@ -1,5 +1,10 @@
 package com.example.test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+
 // Java - Variables and Datatypes
 public class VariablesDatatypes {
 
@@ -176,5 +181,104 @@ public class VariablesDatatypes {
         myArray3[1] = " ";
         myArray3[2] = "World!";
         System.out.println(myArray3[0] + myArray3[1] + myArray3[2]);
+
+        // 자료형 - 리스트
+        System.out.println("== 리스트 ==");
+        ArrayList l1 = new ArrayList();
+        
+        // add
+        l1.add(1);
+        l1.add("hello");
+        l1.add(2);
+        l1.add(3);
+        l1.add(4);
+        l1.add("world!");
+        System.out.println("l1 = " + l1);
+        
+        l1.add(0, 1);
+        System.out.println("l1 = " + l1);
+
+        // get
+        System.out.println(l1.get(0));
+        System.out.println(l1.get(3));
+
+        // size
+        System.out.println(l1.size());
+
+        // remove
+        System.out.println(l1.remove(0));
+        System.out.println("l1 = " + l1);
+
+        System.out.println(l1.remove(l1.indexOf(2)));
+//        System.out.println(l1.remove(l1.remove(Integer.valueOf(2)));
+        System.out.println("l1 = " + l1);
+        
+        // clear 
+        l1.clear();
+        System.out.println("l1 = " + l1);
+
+        // sort
+        ArrayList l2 = new ArrayList();
+        l2.add(5);
+        l2.add(3);
+        l2.add(4);
+        System.out.println("l2 = " + l2);
+        
+        l2.sort(Comparator.naturalOrder());
+        System.out.println("l2 = " + l2);
+        l2.sort(Comparator.reverseOrder());
+        System.out.println("l2 = " + l2);
+
+        // contains
+        System.out.println(l2.contains(1));
+        System.out.println(l2.contains(3));
+
+        // Maps
+        System.out.println("== Map ===");
+        HashMap map = new HashMap();
+
+        // put
+        map.put("kiwi", 9000);
+        map.put("apple", 10000);
+        map.put("mango", 12000);
+        System.out.println("map = " + map);
+
+        // get
+        System.out.println(map.get("mandarine"));
+        System.out.println(map.get("kiwi"));
+
+        // size
+        System.out.println(map.size());
+
+        // remove
+        System.out.println(map.remove("kiwi"));
+        System.out.println(map.remove("mandarine"));
+        System.out.println("map = " + map);
+
+        // containsKey
+        System.out.println(map.containsKey("apple"));
+        System.out.println(map.containsKey("kiwi"));
+
+        // Generics
+        System.out.println("== Generics ==");
+        ArrayList l3 = new ArrayList();
+        l3.add(1);
+        l3.add("hello");
+        System.out.println("l3 = " + l3);
+
+//        ArrayList<String> l4 = ArrayList<String>();
+//        l4.add(1);
+//        l4.add("hello");
+//        System.out.println("l4 = " + l4);
+
+        HashMap map1 = new HashMap();
+        map1.put(123, "id");
+        map1.put("apple", 10000);
+        System.out.println("map1 = " + map1);
+
+        HashMap<String, Integer> map2 = new HashMap<>();
+//        map2.put(123, "id");
+        map2.put("apple", 10000);
+        System.out.println("map2 = " + map2);
     }
 }
