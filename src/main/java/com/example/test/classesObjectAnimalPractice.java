7e1a92f6f676f66a513544321c415ae55dc84de5
Jeongho Park <d0lbangi13@naver.com>
2023-05-25 14:58:57 +0900

    added classesObject with Animal practice

---
+++ src/main/java/com/example/test/classesObjectAnimalPractice.java
@@ -0,0 +1,46 @@
+package com.example.test;
+
+//  Practice
+//  다음 클래스 직접 만든 후 객체 생성
+//  클래스명: Animal
+//  특성: 이름, 무게, 분류
+//  기능: 먹기, 잠자기, 걷기, 뛰기
+
+public class classesObjectAnimalPractice {
+    String name;
+    Double weight;
+    String classification;
+
+    classesObjectAnimalPractice(String name, Double weight, String classification) {
+        this.name=name;
+        this.weight=weight;
+        this.classification=classification;
+    }
+
+    public void printInfo() {
+        System.out.println("name = " + name);
+        System.out.println("weight = " + weight);
+        System.out.println("classification = " + classification);
+    }
+
+    public void eat() {
+        System.out.println("냠냠!");
+    }
+    public void sleep() {
+        System.out.println("쿨쿨!");
+    }
+    public void walk() {
+        System.out.println("걷기!");
+    }
+    public void run() {
+        System.out.println("뛰기!");
+    }
+    public static void main(String[] args) {
+        // Test code
+        classesObjectAnimalPractice animal1 = new classesObjectAnimalPractice("강아지", 5.0, "포유류");
+        classesObjectAnimalPractice animal2 = new classesObjectAnimalPractice("구피", 0.01, "어류");
+//
+        animal1.printInfo();
+        animal2.printInfo();
+    }
+}
