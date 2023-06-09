2852b2cfb962c38e5cef9df14aeb6cd470d4e677
Jeongho Park <d0lbangi13@naver.com>
2023-06-09 19:48:37 +0900

    Completed example of ExponentialAndLogarithmic

---
+++ src/main/java/com/example/BasicMath/PracticeExponentialAndLogarithmic.java
@@ -0,0 +1,54 @@
+package com.example.BasicMath;
+
+// 제곱과 제곱근을 Math 없이 구현하기
+
+public class PracticeExponentialAndLogarithmic {
+
+    static double pow(int a, int b) {
+        double result = 1;
+        boolean isMinus = false;
+
+        if (b==0){
+            return 1;
+        } else if(b < 0) {
+            b *= -1;
+            isMinus = true;
+        }
+
+        for(int i = 0; i < b; i++){
+            result *= a;
+        }
+        return isMinus ? 1 / result : result;
+    }
+
+    static double sqrt(int a){
+        double result = 1;
+
+        for (int i = 0; i < 10; i++){
+            result = (result + (a / result )) / 2;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+        // test code
+        System.out.println("== Math pow == ");
+        System.out.println(Math.pow(2,3)); // 8
+        System.out.println(Math.pow(2,-3)); // 0.125
+        System.out.println(Math.pow(-2, 3)); // -8
+
+        System.out.println("== My pow == ");
+        System.out.println(pow(2,3)); // 8
+        System.out.println(pow(2,-3)); // 0.125
+        System.out.println(pow(-2, 3)); // -8
+
+        System.out.println("== Math sqrt == ");
+        System.out.println(Math.sqrt(16)); // 4
+        System.out.println(Math.sqrt(8)); // 2 route 2
+
+        System.out.println("== Mu sqrt == ");
+        System.out.println(sqrt(16)); // 4
+        System.out.println(sqrt(8)); // 2 route 2
+
+    }
+}
